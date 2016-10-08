77fa86eafa33533da153b3d93c4339724eddea94
aadil <aadil@pocketgems.com>
2016-10-08 19:37:59 -0400

    Twilio v1

---
+++ Twilio_Important.as
@@ -0,0 +1,21 @@
+--Starting
+
+cd Documents/my_tutorial_folder
+source bin/activate
+
+Twilio number +16173077805
+(510) 460-1941 - purchased
+
+Recovery key
+R1LZJxTnLKDwul1tZvtHBNsWqtv0Gn4RuSm0D6ac
+
+How to add something to your bash profile
+echo 'export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin' >>~/.bash_profile
+
+
+How to get into your heroku virtual environment
+virtualenv venv
+source venv/bin/activate
+
+Website
+http://quiet-hollows-27977.herokuapp.com/
---
+++ send_sms.py
@@ -0,0 +1,10 @@
+# Download the twilio-python library from http://twilio.com/docs/libraries
+from twilio.rest import TwilioRestClient
+
+# Find these values at https://twilio.com/user/account
+account_sid = "AC9cae8a357c0111ce160b7410aa7bfe2c"
+auth_token = "66a1c2714748a09d057429d10e0e6346"
+client = TwilioRestClient(account_sid, auth_token)
+
+message = client.messages.create(to="+16175105178", from_="+16173077805",
+                                     body="Hi Sexy", status_callback="http://requestb.in/1234abcd")
